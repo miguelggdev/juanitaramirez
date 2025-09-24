de0480e232ea6830c2768aa14f25c10241314d03
miguelggdev <magonzalezg@udistrital.edu.co>
2025-09-24 10:22:49 -0500

    [dyad] Corregir fondo para video, navbar y footer - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,7 +14,7 @@ import { MadeWithDyad } from "@/components/made-with-dyad";
 
 const Index = () => {
   return (
-    <div className="text-white">
+    <div className="bg-slate-950 text-white">
       <Navbar />
       <main>
         <Hero />
